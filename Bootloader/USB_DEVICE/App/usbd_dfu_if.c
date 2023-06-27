c9d7893cb50dc7497b7ada5825e9b7fbc425331b
Sergey Scherbakov <scherbakov.sergey24@gmail.com>
2023-06-27 16:43:10 +0300

    work done

---
+++ Bootloader/USB_DEVICE/App/usbd_dfu_if.c
@@ -0,0 +1,300 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : usbd_dfu_if.c
+  * @brief          : Usb device for Download Firmware Update.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "usbd_dfu_if.h"
+
+/* USER CODE BEGIN INCLUDE */
+#include "w25qxx_qspi.h"
+/* USER CODE END INCLUDE */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
+  * @brief Usb device.
+  * @{
+  */
+
+/** @defgroup USBD_DFU
+  * @brief Usb DFU device module.
+  * @{
+  */
+
+/** @defgroup USBD_DFU_Private_TypesDefinitions
+  * @brief Private types.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_TYPES */
+
+/* USER CODE END PRIVATE_TYPES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_DFU_Private_Defines
+  * @brief Private defines.
+  * @{
+  */
+
+#define FLASH_DESC_STR      "@Internal Flash   /0x90000000/2048*004Kg"
+
+/* USER CODE BEGIN PRIVATE_DEFINES */
+#define FLASH_ERASE_TIME    (uint16_t)50
+#define FLASH_PROGRAM_TIME  (uint16_t)50
+
+/* USER CODE END PRIVATE_DEFINES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_DFU_Private_Macros
+  * @brief Private macros.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_MACRO */
+
+/* USER CODE END PRIVATE_MACRO */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_DFU_Private_Variables
+  * @brief Private variables.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_VARIABLES */
+uint8_t tmpBuf[4096];
+/* USER CODE END PRIVATE_VARIABLES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_DFU_Exported_Variables
+  * @brief Public variables.
+  * @{
+  */
+
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+/* USER CODE BEGIN EXPORTED_VARIABLES */
+
+/* USER CODE END EXPORTED_VARIABLES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_DFU_Private_FunctionPrototypes
+  * @brief Private functions declaration.
+  * @{
+  */
+
+static uint16_t MEM_If_Init_FS(void);
+static uint16_t MEM_If_Erase_FS(uint32_t Add);
+static uint16_t MEM_If_Write_FS(uint8_t *src, uint8_t *dest, uint32_t Len);
+static uint8_t *MEM_If_Read_FS(uint8_t *src, uint8_t *dest, uint32_t Len);
+static uint16_t MEM_If_DeInit_FS(void);
+static uint16_t MEM_If_GetStatus_FS(uint32_t Add, uint8_t Cmd, uint8_t *buffer);
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_DECLARATION */
+
+/* USER CODE END PRIVATE_FUNCTIONS_DECLARATION */
+
+/**
+  * @}
+  */
+
+#if defined ( __ICCARM__ ) /* IAR Compiler */
+  #pragma data_alignment=4
+#endif
+__ALIGN_BEGIN USBD_DFU_MediaTypeDef USBD_DFU_fops_FS __ALIGN_END =
+{
+   (uint8_t*)FLASH_DESC_STR,
+    MEM_If_Init_FS,
+    MEM_If_DeInit_FS,
+    MEM_If_Erase_FS,
+    MEM_If_Write_FS,
+    MEM_If_Read_FS,
+    MEM_If_GetStatus_FS
+};
+
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Memory initialization routine.
+  * @retval USBD_OK if operation is successful, MAL_FAIL else.
+  */
+uint16_t MEM_If_Init_FS(void)
+{
+  /* USER CODE BEGIN 0 */
+	//HAL_FLASH_Unlock();
+  return (USBD_OK);
+  /* USER CODE END 0 */
+}
+
+/**
+  * @brief  De-Initializes Memory
+  * @retval USBD_OK if operation is successful, MAL_FAIL else
+  */
+uint16_t MEM_If_DeInit_FS(void)
+{
+  /* USER CODE BEGIN 1 */
+	//HAL_FLASH_Lock();
+  return (USBD_OK);
+  /* USER CODE END 1 */
+}
+
+/**
+  * @brief  Erase sector.
+  * @param  Add: Address of sector to be erased.
+  * @retval 0 if operation is successful, MAL_FAIL else.
+  */
+uint16_t MEM_If_Erase_FS(uint32_t Add)
+{
+  /* USER CODE BEGIN 2 */
+
+	if (Add < 0x90000000 || Add >= 0x90800000)
+		return -1;
+	Add -= QSPI_BASE;
+	W25q_Erase_4k(Add);
+	return 0;
+  /* USER CODE END 2 */
+}
+
+/**
+  * @brief  Memory write routine.
+  * @param  src: Pointer to the source buffer. Address to be written to.
+  * @param  dest: Pointer to the destination buffer.
+  * @param  Len: Number of data to be written (in bytes).
+  * @retval USBD_OK if operation is successful, MAL_FAIL else.
+  */
+uint16_t MEM_If_Write_FS(uint8_t *src, uint8_t *dest, uint32_t Len)
+{
+  /* USER CODE BEGIN 3 */
+	uint32_t address = ((uint32_t)dest);
+	if (address < 0x90000000 || address >= 0x90800000)
+		return -1;
+	address -= QSPI_BASE;
+
+	while (Len >= 4096) {
+		W25q_Write_4k(address, src);
+		address += 4096;
+		src += 4096;
+		Len -= 4096;
+	}
+
+	if (Len > 0) {
+		memset(tmpBuf, 0xFF, 4096);
+		memcpy(tmpBuf, src, Len);
+		W25q_Write_4k(address, tmpBuf);
+	}
+	return 0;
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief  Memory read routine.
+  * @param  src: Pointer to the source buffer. Address to be written to.
+  * @param  dest: Pointer to the destination buffer.
+  * @param  Len: Number of data to be read (in bytes).
+  * @retval Pointer to the physical address where data should be read.
+  */
+uint8_t *MEM_If_Read_FS(uint8_t *src, uint8_t *dest, uint32_t Len)
+{
+  /* Return a valid address to avoid HardFault */
+  /* USER CODE BEGIN 4 */
+	uint32_t address = ((uint32_t)src);
+	if (address < 0x90000000 || address >= 0x90800000) {
+		memset(dest, 0xEE, Len);
+		return (uint8_t *) (dest);
+	}
+	address -= QSPI_BASE;
+
+	uint8_t* d = dest;
+	while (Len >= 4096) {
+		W25q_Read_4k(address, dest);
+		address += 4096;
+		dest += 4096;
+		Len -= 4096;
+	}
+
+	if (Len > 0) {
+		W25q_Read_4k(address, tmpBuf);
+		memcpy(dest, tmpBuf, Len);
+	}
+
+	/* Return a valid address to avoid HardFault */
+	return (uint8_t *) (d);
+  /* USER CODE END 4 */
+}
+
+/**
+  * @brief  Get status routine
+  * @param  Add: Address to be read from
+  * @param  Cmd: Number of data to be read (in bytes)
+  * @param  buffer: used for returning the time necessary for a program or an erase operation
+  * @retval USBD_OK if operation is successful
+  */
+uint16_t MEM_If_GetStatus_FS(uint32_t Add, uint8_t Cmd, uint8_t *buffer)
+{
+  /* USER CODE BEGIN 5 */
+	switch (Cmd)
+	  {
+	  case DFU_MEDIA_PROGRAM:
+	    buffer[1] = (uint8_t) 50;
+	    buffer[2] = (uint8_t) (50 << 8);
+	    buffer[3] = 0;
+	    break;
+
+	  case DFU_MEDIA_ERASE:
+	  default:
+	    buffer[1] = (uint8_t) 50;
+	    buffer[2] = (uint8_t) (50 << 8);
+	    buffer[3] = 0;
+	    break;
+	  }
+	  return 0;
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_IMPLEMENTATION */
+
+/* USER CODE END PRIVATE_FUNCTIONS_IMPLEMENTATION */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
